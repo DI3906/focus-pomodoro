01c5a2a789fc08d85b952a102e3e9228d1840c26
Diego Garcia <diegg066.99@gmail.com>
2026-06-13 22:28:51 -0600

    add to-do and pomodoro sections, first version

---
+++ app/components/Pomodoro/Pomodoro.tsx
@@ -0,0 +1,15 @@
+
+export default function Pomodoro() {
+  return (
+    <div className="w-full h-full rounded-2xl p-3 flex-col justify-items-center content-center pomodoro">
+      <p className="text-9xl">25:00</p>
+      <div className="flex">
+        <p>+25</p>
+        <p>+10</p>
+        <p>+5</p>
+        <p>+1</p>
+      </div>
+      <p>START</p>
+    </div>
+  );
+}
---
+++ app/components/Todo/Todo.tsx
@@ -0,0 +1,8 @@
+
+export default function Todo() {
+  return(
+    <div className="w-full h-full rounded-2xl p-3 todo">
+      <p className="">create function for todo</p>
+    </div>
+  );
+}
