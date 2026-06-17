af6895f8c65a0ce73fdcb1b925e449c9e7f5d158
DI3906 <diegg066.99@gmail.com>
2026-06-16 22:04:06 -0600

    component To-do v1

--- app/components/Todo/Todo.tsx
+++ app/components/Todo/Todo.tsx
@@ -1,8 +1,60 @@
+'use client';
+import { useState } from "react";
+import Modal from "../Modal/Modal";
+import Task from "../Task/Task";
+
+const taskTest = [
+  // {
+  //   id: 1,
+  //   description: "Agregar tarea individual",
+  //   completed: false
+  // },
+  // {
+  //   id: 2,
+  //   description: "Agregar icono para no completado",
+  //   completed: false
+  // },
+  // {
+  //   id: 3,
+  //   description: "Agregar icono para completado",
+  //   completed: false
+  // },
+  // {
+  //   id: 4,
+  //   description: "Agregar state para el cambio entre completado y no completado",
+  //   completed: false
+  // },
+  // {
+  //   id: 5,
+  //   description: "Agregar estilo para completado",
+  //   completed: false
+  // },
+]
 
 export default function Todo() {
-  return(
-    <div className="w-full h-full rounded-2xl p-3 todo">
-      <p className="">create function for todo</p>
+  const [showModalAdd, setShowModalAdd] = useState(false);
+
+  function openModal(){
+    setShowModalAdd(true);
+  }
+
+  return (
+    <div className="w-full h-full rounded-2xl p-3 flex-col justify-items-center content-center todo">
+      {taskTest === undefined || taskTest.length == 0 ? (
+        <div className="w-full flex flex-col items-center">
+          <p className="font-(family-name:--brygada) text-(--blue)">no task for today</p>
+          <button className="add-task" onClick={openModal}>add new task</button>
+        </div>
+      ) : (
+        taskTest.map((task) =>
+          <Task id={task.id} detail={task.description} completed={task.completed} />
+        )
+
+      )}
+      <Modal
+        isShow={showModalAdd}
+        description="add task"
+      />
     </div>
   );
 }
