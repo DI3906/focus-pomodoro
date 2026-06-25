0ac9e1b03029465a038acb4f65c81e567ed536de
DI3906 <diegg066.99@gmail.com>
2026-06-24 18:57:27 -0600

    update style for Task component

--- app/components/Task/Task.tsx
+++ app/components/Task/Task.tsx
@@ -1,16 +1,26 @@
+import { SquareDashed, SquareCheckBig, Trash2 } from "lucide-react";
 
 type TaskProps = {
   id: number,
   detail: string,
-  completed: boolean,
+  isCompleted: boolean,
 }
 
-export default function Task({id, detail, completed}: TaskProps) {
-
+export default function Task({id, detail, isCompleted}: TaskProps) {
   return (
-    <div className="flex">
-      <span>icon</span>
-      <p>{detail}</p>
-    </div>
+    <li id={`item-${id}`} className="flex w-full">
+      {isCompleted ? (
+        <div className="flex items-start w-full">
+          <SquareCheckBig className="shrink-0" />
+          <p className="flex-1 line-through mx-3">{detail}</p>
+          <Trash2 className="shrink-0" color="#BE5E00"/>
+        </div>
+      ) : (
+        <div className="flex items-start w-full">
+          <SquareDashed className="shrink-0" />
+          <p className="ml-3 mr-6">{detail}</p>
+        </div>
+      )}
+    </li>
   );
 }
